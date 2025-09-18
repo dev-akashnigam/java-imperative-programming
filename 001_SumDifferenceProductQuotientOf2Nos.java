bd1e3d09918e2087f7abbcbd24ade75224077b09
Akash Nigam <contact.akashnigam@gmail.com>
2025-09-18 16:40:33 +0530

    Imperative Java solution for printing the sum, difference, product and quotient of two numbers entered by the user

---
+++ 001_SumDifferenceProductQuotientOf2Nos.java
@@ -0,0 +1,27 @@
+// Read two numbers and print their sum, difference, product, and quotient.
+
+import java.util.Scanner;
+
+class SumDifferenceProductQuotientOf2Nos {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int firstNum = scanner.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int secondNum = scanner.nextInt();
+
+        int sum = firstNum + secondNum;
+        int diff = Math.abs(firstNum - secondNum);
+        int product = firstNum * secondNum;
+        int quotient = firstNum/secondNum;
+
+        System.out.printf("Sum = %d%n", sum);
+        System.out.printf("Difference = %d%n", diff);
+        System.out.printf("Product = %d%n", product);
+        System.out.printf("Quotient = %d%n", quotient);
+
+        scanner.close();
+    }
+}
